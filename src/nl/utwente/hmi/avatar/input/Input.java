2719e62d41015fecf6b3a6226d5aefaa35c11f62
Bouke Regnerus <bouke@regner.us>
2017-04-04 16:16:50 +0200

    Speech API Fix

--- src/nl/utwente/hmi/avatar/input/Input.java
+++ src/nl/utwente/hmi/avatar/input/Input.java
@@ -4,14 +4,8 @@ package nl.utwente.hmi.avatar.input;
  * Created by Bouke on 04/04/2017.
  */
 public class Input {
-    private Input input;
 
-    public Input(String[] args) {
-        String method = args[0];
-        String language = args[1];
+    public Input() {
 
-        if (method == "speech") {
-            this.input = new SpeechAPI(language, null);
-        }
     }
 }
