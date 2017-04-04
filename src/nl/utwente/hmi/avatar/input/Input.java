777d9c53c604e9a733338c6395e68fd85de3c62e
Bouke Regnerus <bouke@regner.us>
2017-04-04 16:56:13 +0200

    Added Speech Recognition Fix Mac

--- src/nl/utwente/hmi/avatar/input/Input.java
+++ src/nl/utwente/hmi/avatar/input/Input.java
@@ -4,8 +4,12 @@ package nl.utwente.hmi.avatar.input;
  * Created by Bouke on 04/04/2017.
  */
 public class Input {
-
     public Input() {
 
+    }
+
+    //launch app
+    public static void main(String[] args) {
+        new SpeechAPI("nlspraak.ewi.utwente.nl:8889", null);
     }
 }
